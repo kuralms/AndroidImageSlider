69789c82a8efe2aa54ad33184c85b2cc77fc9b35
kuralv.alt <kuralv.alt@ext.zeta.tech>
2021-08-19 12:09:54 +0530

    test

--- demo/src/main/java/com/daimajia/slider/demo/MainActivity.java
+++ demo/src/main/java/com/daimajia/slider/demo/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends ActionBarActivity implements BaseSliderView.On
 
         HashMap<String,String> url_maps = new HashMap<String, String>();
         url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
-        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("Big Bang Theory---", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
         url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
         url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
 
